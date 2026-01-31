ab4ed7a21103320d5b0fde48fda3fee2ede83859
George Gil <georgegil3902@gmail.com>
2026-01-31 21:13:39 +0100

    Added Transmission manager service id

---
+++ include/hawkeye/services/service_ids.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <cstdint>
+
+namespace hawkeye::services {
+
+    // Stable ID + version for TransmissionService
+    static constexpr uint64_t TRANSMISSION_SERVICE = 0x48574B595F545853ULL; // "HWKY_TXS"
+    static constexpr uint32_t TRANSMISSION_SERVICE_V1 = 1;
+
+}
